0631e296e3f49f6ee464ea3ad7bfa8007f163311
Jan Everaert <jan@crashlab.be>
2022-02-14 15:30:06 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function init() {
+    console.log("Init");
+}
+
+function pullLever() {
+
+}
+
+function showResult() {
+
+}
+
+function showStatus() {
+    
+}
+
+const slotMachine = {
+    calculateStatus: function() {
+    },
+    getRandomSymbol: function() {
+    },
+    reset: function() {
+    },
+    symbols: [ "♠", "♥", "♣", "♦"],
+    slots: [],
+    slotsAmount: 3,
+    spin: function() {
+    },
+    win: true
+};
+
+init();
